464a568d66663e2241d4aef9c0a07814b56a0f20
John Matter <jmatter4@gmail.com>
2026-09-03 23:19:28 -0400

    abbreviations: Add cs for the CoilSnake CLI

--- conf.d/abbreviations.fish
+++ conf.d/abbreviations.fish
@@ -5,6 +5,7 @@ abbr -a tw timew
 abbr -a tv tovault
 abbr -a lg lazygit
 abbr -a td vault todo
+abbr -a cs /Users/matter/Games/earthbound/CoilSnake/bin/coilsnake-cli
 abbr -a git-branch 'git log --oneline --graph --all --decorate --simplify-by-decoration'
 abbr -a dot 'git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 abbr -a yt-dlp-wav 'yt-dlp -x --audio-format wav'
